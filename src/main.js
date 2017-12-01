cdd505be934e65d8fef4c400634acec976e42334
n6134123 <n6134123@gmail.com>
2017-11-30 17:36:33 -0800

    log in settings

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ export class Main extends React.Component {
     render() {
         return (
             <div className="main">
-            <WrappedRegister/>
+                <WrappedRegister/>
             </div>
         );
     }
